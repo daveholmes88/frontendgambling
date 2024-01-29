b3b3ca36b02bcad469f662a9b200fa02b2be5496
Dave Holmes <daveholmes88@gmail.com>
2024-01-29 14:29:30 -0600

    added information to show scoring percentages for upcoming games

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import moment from 'moment'
 
 
 function App() {
-  const [games, setGames] = useState([])
-  const [date, setDate] = useState('')
+  const [games, setGames] = useState([]);
+  const [date, setDate] = useState('');
+  const [today, setToday] = useState(true);
+  const [data, setData] = useState([0]);
 
   useEffect(() => {
     const d = moment()
@@ -27,23 +29,34 @@ function App() {
     </>;
   }
 
-  const getGames = () => {
-    fetch(`https://www.balldontlie.io/api/v1/games?dates[]=${date}'`)
-      .then(resp => resp.json())
-      .then(data => {
-        const teams = data.data.map(game => {
-          const home = game.home_team.name
-          const away = game.visitor_team.name
-          const gameId = game.id
-          return {home: {name: home}, away: {name: away}, gameId}
-        })
-        setGames(teams)
+  const getGames = async () => {
+    const dateResp = await fetch(`https://www.balldontlie.io/api/v1/games?dates[]=${date}'`)
+    const gameData = await dateResp.json()
+    const games = gameData.data
+    const teams = games.map(game => {
+      const home = game.home_team.name
+      const away = game.visitor_team.name
+      const gameId = game.id
+      return {home: {name: home}, away: {name: away}, gameId}
+    })
+    const notPlayed = games[0].home_team_score === 0
+    const gamesData = []
+    if (notPlayed) {
+      const teamResp = await fetch('http://localhost:3000/teams')
+      const teamData = await teamResp.json()
+      teamData.forEach(gameData => {
+        const playingToday = teams.find(team => team.home.name === gameData.name || team.away.name === gameData.name)
+        if (playingToday) gamesData.push(gameData)
       })
+    }
+        setToday(notPlayed)
+        setGames(teams)
+        setData(gamesData)
   }
 
   const submit = () => {
     const options = {
-      method: 'PUT',
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
@@ -52,13 +65,27 @@ function App() {
     fetch('http://localhost:3000/teams', options)
   }
 
-  console.log('games', games)
+  const showData = () => {
+    return games.map(game => {
+      const home = data.find(team => game.home.name === team.name);
+      const away = data.find(team => game.away.name === team.name);
+      const homeScore = (home.off_score/home.games_played + away.def_score/away.games_played) / 2;
+      const awayScore = (home.def_score/home.games_played + away.off_score/away.games_played) / 2;
+      return <>
+      <h4>{home.name}: Offense: {homeScore.toFixed(2)}</h4>
+      <h4>{away.name}: Offense: {awayScore.toFixed(2)}</h4>
+      <p>_____________________________________________</p>
+      <br></br>
+      </>;
+    })
+  }
   return (
     <Container>
       <br></br>
     <FormControl>
     <TextField id="outlined-basic" label="Date" variant="outlined" value={date} onChange={(e) => setDate(e.target.value)} />
     <Button onClick={getGames} variant='outlined'>Go</Button>
+      {today ? showData() : <>
       {games.map(game => {
         return <FormGroup>
           {insertCheckboxes(game, 'home')}
@@ -68,6 +95,7 @@ function App() {
         </FormGroup>
       })}
       <Button onClick={submit} variant='outlined'>Submit</Button>
+      </>}
     </FormControl>
     </Container>
   )
